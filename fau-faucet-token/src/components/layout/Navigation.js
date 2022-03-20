12c37e4ebcd26771dea18b738cd8c57cc9bbbc61
Duc Nguyen <emngok123@gmail.com>
2022-03-20 14:59:32 +0700

    move mobile navigation into the parent view

--- fau-faucet-token/src/components/layout/Navigation.js
+++ fau-faucet-token/src/components/layout/Navigation.js
@@ -51,16 +51,19 @@ function Navigation() {
 
                     }
                 </div>
+
+                <div id="navDemo" className="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large">
+                    <a href="#" className="w3-bar-item w3-button w3-padding-large">Swap</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding-large">Pools</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding-large">Staking</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding-large">Faucet</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding-large">NFTs</a>
+                    <a href="#" className="w3-bar-item w3-button w3-padding-large">Wrap NEAR</a>
+                </div>
+
             </div>
 
-            <div id="navDemo" className="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large">
-                <a href="#" className="w3-bar-item w3-button w3-padding-large">Swap</a>
-                <a href="#" className="w3-bar-item w3-button w3-padding-large">Pools</a>
-                <a href="#" className="w3-bar-item w3-button w3-padding-large">Staking</a>
-                <a href="#" className="w3-bar-item w3-button w3-padding-large">Faucet</a>
-                <a href="#" className="w3-bar-item w3-button w3-padding-large">NFTs</a>
-                <a href="#" className="w3-bar-item w3-button w3-padding-large">Wrap NEAR</a>
-            </div>
+
         </>
 
     )
