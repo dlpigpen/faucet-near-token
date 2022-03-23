128259ba1fa0ddfcee7c4c9ea7afa65ec098b608
Duc Nguyen <emngok123@gmail.com>
2022-03-23 12:51:35 +0700

    add footer

--- fau-faucet-token/src/components/layout/Footer.js
+++ fau-faucet-token/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className="w3-bottom w3-container w3-center w3-margin-top w3-xlarge">
+        <footer className="w3-bottom w3-container w3-center w3-margin-top w3-xlarge w3-hide-small">
             <p className="w3-large">Find me on social media.</p>
             <div className="w3-row">
             <a href="https://t.me/mitsori" target="_blank">
@@ -17,7 +17,7 @@ function Footer() {
             <a href="https://github.com/dlpigpen" target="_blank">
                 <i className="fa fa-github w3-hover-opacity w3-text-theme"></i>
             </a>
-            
+
             </div>
             <p className="w3-small">Powered by <a href="https://github.com/dlpigpen" target="_blank">MD</a></p>
             
