f4a57f3049eb3a46bb291b1cbac1e19363b5902a
Duc Nguyen <emngok123@gmail.com>
2022-04-04 11:40:56 +0700

    add migrate state

--- fau-faucet-token/contract/src/lib.rs
+++ fau-faucet-token/contract/src/lib.rs
@@ -71,12 +71,40 @@ impl FaucetContract {
          }
     }
 
+    #[init(ignore_state)]
+    pub fn migrate_state() -> Self {
+        // Deserialize the state using the old contract structure.
+        let old_contract: FaucetContract = env::state_read().expect("Old state doesn't exist");
+        // Verify that the migration can only be done by the owner.
+        // This is not necessary, if the upgrade is done internally.
+        log!("predecessor_account_id: {}", &env::predecessor_account_id());
+        log!("&old_contract.owner_id: {}",  &old_contract.owner_id);
+        assert_eq!(
+            &env::predecessor_account_id(),
+            &old_contract.owner_id,
+            "Can only be called by the owner"
+        );
+
+        // Create the new contract using the data from the old contract.
+        Self { 
+            owner_id: old_contract.owner_id, 
+            ft_contract_id: old_contract.ft_contract_id, 
+            total_balance_share: old_contract.total_balance_share, 
+            total_shared: old_contract.total_shared, 
+            total_account_shared: old_contract.total_shared, 
+            accounts: old_contract.accounts,
+            max_share_per_account: old_contract.max_share_per_account,
+            is_paused: old_contract.is_paused,
+         }
+    }
+
+
     #[payable]
     pub fn faucet_token(&mut self, amount: U128) -> Promise {
         assert!(env::attached_deposit() > 1, "ERR_DEPOSIT_GREATER_THAN_ONE_YOCTO");
         let account_id: AccountId = env::predecessor_account_id();
         let account_balance: Balance = self.accounts.get(&account_id).unwrap_or_else(|| 0);
-        assert!(account_balance + amount.0 <= self.max_share_per_account, "ERR_INVALID_AMOUNT");
+        assert!(amount.0 <= self.max_share_per_account, "ERR_INVALID_AMOUNT");
 
         ext_ft_contract::ft_transfer(
             account_id.clone(), 
